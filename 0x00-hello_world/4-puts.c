16f57943dc8f656c53e97db5d0292fcbc7c454fe
ibamlak <bksami09@gmail.com>
2022-09-12 03:36:20 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+
+/**
+ * main - print the string in the dput function 
+ *
+ * Description: using the main function
+ * this program prints "programming is like building a multiligual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"programming is like building a multiligual puzzle");
+	return (0);
+}
+
