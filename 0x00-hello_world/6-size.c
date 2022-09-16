fc3bda3165e2c7c7eaa6af885474d5d1ed0b159d
ibamlak <bksami09@gmail.com>
2022-09-16 11:15:08 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@
  * this program prints the size of a variable's value
  * Return: 0
  */
-int main (void)
+int main(void)
 {
 	char c;
 	int i;
@@ -15,10 +15,10 @@ int main (void)
 	long long lli;
 	float f;
 
-	printf("size of a char: %d byte(s)\n", sizeof(c));
-	printf("size of an int: %d byte(s)\n", sizeof(i));
-	printf("size of a long int: %d byte(s)\n", sizeof(li));
-	printf("size of a long long int: %d byte(s)\n", sizeof(lli));
-	printf("size of float: %d byte(s)\n", sizeof(f));
+	puts("size of a char: %d byte(s)\n", sizeof(c));
+	puts("size of an int: %d byte(s)\n", sizeof(i));
+	puts("size of a long int: %d byte(s)\n", sizeof(li));
+	puts("size of a long long int: %d byte(s)\n", sizeof(lli));
+	puts("size of float: %d byte(s)\n", sizeof(f));
 	return (0);
 }
