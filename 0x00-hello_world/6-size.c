bf015a6e77d2206ce638e44ccc4d17ca3437677b
ibamlak <bksami09@gmail.com>
2022-09-16 11:17:37 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,10 +15,10 @@ int main(void)
 	long long lli;
 	float f;
 
-	puts("size of a char: %d byte(s)\n", sizeof(c));
-	puts("size of an int: %d byte(s)\n", sizeof(i));
-	puts("size of a long int: %d byte(s)\n", sizeof(li));
-	puts("size of a long long int: %d byte(s)\n", sizeof(lli));
-	puts("size of float: %d byte(s)\n", sizeof(f));
+	printf("size of a char: %d byte(s)\n", sizeof(c));
+	printf("size of an int: %d byte(s)\n", sizeof(i));
+	printf("size of a long int: %d byte(s)\n", sizeof(li));
+	printf("size of a long long int: %d byte(s)\n", sizeof(lli));
+	printf("size of float: %d byte(s)\n", sizeof(f));
 	return (0);
 }
