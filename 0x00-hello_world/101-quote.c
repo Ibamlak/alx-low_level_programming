1ab14ef555a8b0957c197766bd9421806991312e
ibamlak <bksami09@gmail.com>
2022-09-21 13:27:40 -0700

    shell variables expansion

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <unistd.h>
 
-
 /**
  * main - prints a text 
  * Returns: always 1 (success)
  */
 int main(void)
 {
-	write(2,"and that piece of art is usefule\"-Dora Korpar,2015-10-19\n",59);
+	write(2,"and that piece of art is useful\"-Dora Korpar, 2015-10-19\n");
 	return (1);
 }
